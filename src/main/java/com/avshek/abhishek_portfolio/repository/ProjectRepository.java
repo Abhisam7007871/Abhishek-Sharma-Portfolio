52112802508e05317564a4cf55f502ee75f1d034
Abhishek <abhisam7007871@gmail.com>
2024-10-07 10:52:02 +0530

    Initial Commit

--- src/main/java/com/avshek/abhishek_portfolio/repository/ProjectRepository.java
+++ src/main/java/com/avshek/abhishek_portfolio/repository/ProjectRepository.java
@@ -3,5 +3,6 @@ package com.avshek.abhishek_portfolio.repository;
 import com.avshek.abhishek_portfolio.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
